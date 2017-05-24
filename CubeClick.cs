41922a13875bbf199e86edaaf961ac3777972d93
qwerty348810 <Dmitrii.yakovlev@mail.ru>
2017-05-24 21:51:28 +0300

    Signed-off-by: qwerty348810 <Dmitrii.yakovlev@mail.ru>

--- CubeClick.cs
+++ CubeClick.cs
@@ -1,35 +1,22 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-public class CubeClick : MonoBehaviour {
-    float timerRemaining = 5;
-    bool count = false;
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (count == true) {
-            timerRemaining -= Time.deltaTime;
-        }
-        
-    }
-    private void OnMouseDown(){
-        count = true;
-        Debug.Log ("Test 2!!!!!!!");
-	}
-    void OnGUI()
-    {
-        if (timerRemaining > 0)
-        {
-            GUI.Label(new Rect(100, 100, 200, 100), "Time Ramaining:" + timerRemaining);
-        }
-        else
-        {
-            GUI.Label(new Rect(100, 100, 100, 100), "Time s UP");
-        }
-    }
+public class CubeClick : MonoBehaviour
+{
+    public GameObject EnableDisable;
+    bool click = true; 
 
+    
+    private void OnMouseDown(){
+        if (click == true)
+        {
+            EnableDisable.SetActive(true);
+            click = false;
+        }
+        if (click == false)
+        {
+            EnableDisable.SetActive(false);
+            click = true;
+        }
+    }
 }
---
+++ EnableDis.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableDis : MonoBehaviour {
+    public GameObject EnableDisable;
+    public void Enable()
+    {
+        EnableDisable.SetActive(true);
+    }
+    public void Disable()
+    {
+        EnableDisable.SetActive(false);
+    }
+}

e0a7f92e48fffbbd85e0517d3159f422f26c7749
qwerty348810 <Dmitrii.yakovlev@mail.ru>
2017-05-24 18:15:16 +0300

    Signed-off-by: qwerty348810 <Dmitrii.yakovlev@mail.ru>

---
+++ CubeClick.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CubeClick : MonoBehaviour {
+    float timerRemaining = 5;
+    bool count = false;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (count == true) {
+            timerRemaining -= Time.deltaTime;
+        }
+        
+    }
+    private void OnMouseDown(){
+        count = true;
+        Debug.Log ("Test 2!!!!!!!");
+	}
+    void OnGUI()
+    {
+        if (timerRemaining > 0)
+        {
+            GUI.Label(new Rect(100, 100, 200, 100), "Time Ramaining:" + timerRemaining);
+        }
+        else
+        {
+            GUI.Label(new Rect(100, 100, 100, 100), "Time s UP");
+        }
+    }
+
+}

4a54e59ffbfcddb59a7cab2fe046cd1db73c5095
qwerty348810 <Dmitrii.yakovlev@mail.ru>
2017-05-26 10:21:38 +0300

    Signed-off-by: qwerty348810 <Dmitrii.yakovlev@mail.ru>

--- CubeClick.cs
+++ CubeClick.cs
@@ -1,22 +1,59 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CubeClick : MonoBehaviour
 {
-    public GameObject EnableDisable;
-    bool click = true; 
+    public GameObject MySphere;
+    
+    public Text MyText_2;
+    public bool click = true;
+    float timerRemaining = 5;
+    bool count = false;
+    public float score = 0;
+
+    void Start()
+    {
+        
+    }
 
     
-    private void OnMouseDown(){
+    void Update()
+    {
+        if (count == true)
+        {
+            timerRemaining -= Time.deltaTime;
+        }
+        
+    }
+    private void OnMouseDown()
+    {
+        count = true;
         if (click == true)
         {
-            EnableDisable.SetActive(true);
+            Debug.Log("Spher Invis");
+            MySphere.SetActive(false);
             click = false;
-        }
-        if (click == false)
+        }       
+    }
+    void OnGUI()
+    {
+        if (timerRemaining > 0)
         {
-            EnableDisable.SetActive(false);
+            GUI.Label(new Rect(100, 100, 200, 100), "Time Ramaining:" + timerRemaining);
+        }
+        else
+        {
+            GUI.Label(new Rect(100, 100, 100, 100), "Time s UP");
+            count = false;
+            timerRemaining = 5;
+            Debug.Log("Spher Visibl");
+            MySphere.SetActive(true);
             click = true;
+            score++;
+            MyText_2.text = score.ToString("f0");
+
+
         }
     }
-}
+   }

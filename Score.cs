6190cabb5ac5619df6ecfedd3243aba8e703a873
qwerty348810 <Dmitrii.yakovlev@mail.ru>
2017-05-28 12:20:20 +0300

    Popur and Buy_Cube - deal

--- Score.cs
+++ Score.cs
@@ -7,6 +7,10 @@ public class Score : MonoBehaviour
 {
     public CubeClick cubclick_1;
     public CubeClick cubclick_2;
+
+    public Buy_Cube buy_cube;
+
+
     public float scoreGlobal;
     public Text MyText;
 
@@ -16,10 +20,34 @@ public class Score : MonoBehaviour
     
 
 	void Update () {
-        scoreGlobal = cubclick_1.score_1 + cubclick_2.score_1;
+        scoreGlobal = cubclick_1.score_1 + cubclick_2.score_1 + buy_cube.deletScore;
+        
+        
+
+
     }
     void OnGUI()
     {       
         MyText.text = "Score: " + scoreGlobal.ToString();
+    }
+
+
+
+    
+    public float ScoreGlobal
+    {
+        get
+        {
+            return scoreGlobal;
+        }
+        set
+        {
+            scoreGlobal = value;
+        }
+    }
+
+    public float GetScoreGlobal()
+    {
+        return this.scoreGlobal;
     }
 }
---
+++ TestScore.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScore : MonoBehaviour
+{
+    private CubeClick cubclick_1;
+    private CubeClick cubclick_2;
+    public Score score;
+    
+    private Player player = new Player();
+    void Start()
+    {
+        // exemle #1 
+        // player.Power = 150;
+        // player.Power = 150;
+        //Debug.Log("Score: " + player.GetPower());
+        Debug.Log("Score: " + score.GetScoreGlobal());
+    }
+    void Update()
+    {
+        //player.Power = cubclick_1.score_1; //+ cubclick_2.score_1;
+        Debug.Log("Score: " + score.GetScoreGlobal());
+    }
+}
+	public class Player
+    {
+    // exemle #2 
+    // private int power = 150;
+    private float power;
+        public float Power
+        {
+            get {
+                return power;
+            }
+            set {
+                power = value;
+            }
+        }
+    
+    public float GetPower()
+    {
+        return this.power;
+    }
+	
+}
